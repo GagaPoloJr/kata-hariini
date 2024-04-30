bd4eeda23dcbc9a3b55b56934df9e158bf6f9a5c
hklrahmadi <haikalrahmadi@kecilin.id>
2024-05-01 03:23:49 +0700

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Button from "./components/Button";
 import { wordsTodayData } from "./data/data";
@@ -8,15 +8,25 @@ function App() {
   const [wordsToday] = useState(wordsTodayData);
   const [resultWord, setResultWord] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [loadingImage, setLoadingImage] = useState(false);
+  const [imageUrl, setImageUrl] = useState("");
 
-  const generateWords = () => {
+  const generateWords = async () => {
     setIsLoading(true);
+    setLoadingImage(false);
+    setImageUrl("");
     setTimeout(() => {
       const results = wordsToday[Math.floor(Math.random() * wordsToday.length)];
       setResultWord(results);
+
+      // Generate a new random image URL
+      setImageUrl(`https://picsum.photos/500/300?random=${Math.random()}`);
+
       setIsLoading(false);
+      setLoadingImage(true);
     }, 1000);
   };
+
   return (
     <>
       <div className="main h-screen bg-slate-900">
@@ -30,7 +40,23 @@ function App() {
               <Button text="Generate" handleClick={generateWords} />
             </div>
             <div className="mt-10 text-slate-100 font-bold text-center">
-              {!isLoading && resultWord}
+              {!isLoading && loadingImage && (
+                <>
+                  <div className="m-5 lg:m-0 relative w-400 h-300 bg-grey-500">
+                    <img
+                      className="w-full h-full object-cover"
+                      src={imageUrl}
+                      alt=""
+                    />
+                    <div className="absolute inset-0 bg-black opacity-50"></div>
+                    <div className="absolute inset-0 flex items-center justify-center">
+                      <p className="text-white text-lg lg:text-2xl font-bold">
+                        {resultWord}
+                      </p>
+                    </div>
+                  </div>
+                </>
+              )}
             </div>
           </div>
         </div>
--- src/data/data.ts
+++ src/data/data.ts
@@ -2,6 +2,5 @@ export const wordsTodayData = [
     "Gitar yang dipetik, hati yang bergetar 🥶",
     "Jama'ah jum'at yang sedang berbahagia 😬",
     "Urip iku urup (Menyala abangku) 🔥🔥",
-    "Ingin kupukul mas teguh",
     "Tetap menyerah jangan pernah semangat",
 ]
