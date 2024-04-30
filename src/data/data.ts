4ca8476b57c4c60ded0b07efabae97efcd39e9b9
hklrahmadi <haikalrahmadi@kecilin.id>
2024-05-01 02:41:40 +0700

    feat: update data kata kata hari ini

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,5 +1,7 @@
 export const wordsTodayData = [
     "Gitar yang dipetik, hati yang bergetar 🥶",
     "Jama'ah jum'at yang sedang berbahagia 😬",
-    "Urip iku urup (Menyala abangku) 🔥🔥"
+    "Urip iku urup (Menyala abangku) 🔥🔥",
+    "Ingin kupukul mas teguh",
+    "Tetap menyerah jangan pernah semangat",
 ]
