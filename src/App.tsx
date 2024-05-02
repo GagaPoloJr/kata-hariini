dada45031cd75036f7f416d5ca6f7c61c17b30e5
hklrahmadi <haikalrahmadi@kecilin.id>
2024-05-02 10:09:58 +0700

    remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import Button from "./components/Button";
 import { wordsTodayData } from "./data/data";
